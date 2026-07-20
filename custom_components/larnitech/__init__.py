9e25c19ea4c2e7e28138393dcb716ded1373ded6
G1NiS <271513352+G1NiS@users.noreply.github.com>
2026-07-20 07:56:50 +0300

    Clean up stale hidden low-level entities

--- custom_components/larnitech/__init__.py
+++ custom_components/larnitech/__init__.py
@@ -8,11 +8,46 @@ from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers.typing import ConfigType
 
-from .const import DOMAIN, PLATFORMS, TYPE_SWITCH
+from .const import (
+    DOMAIN,
+    PLATFORMS,
+    TYPE_DIMMER,
+    TYPE_DOOR,
+    TYPE_FANCOIL,
+    TYPE_HUMIDITY,
+    TYPE_ILLUMINATION,
+    TYPE_LAMP,
+    TYPE_LEAK,
+    TYPE_LIGHT,
+    TYPE_LIGHT_SCHEME,
+    TYPE_MOTION,
+    TYPE_SWITCH,
+    TYPE_TEMPERATURE,
+    TYPE_VALVE,
+    TYPE_VALVE_HEATING,
+)
+from .entity import entity_enabled_default
 from .hub import LarnitechHub
 from .models import LarnitechDevice
 
 _LOGGER = logging.getLogger(__name__)
+
+TYPE_TO_ENTITY_DOMAIN = {
+    TYPE_LAMP: "light",
+    TYPE_LIGHT: "light",
+    TYPE_DIMMER: "light",
+    TYPE_FANCOIL: "fan",
+    TYPE_TEMPERATURE: "sensor",
+    TYPE_HUMIDITY: "sensor",
+    TYPE_ILLUMINATION: "sensor",
+    TYPE_MOTION: "binary_sensor",
+    TYPE_DOOR: "binary_sensor",
+    TYPE_LEAK: "binary_sensor",
+    TYPE_SWITCH: "switch",
+    TYPE_VALVE: "switch",
+    TYPE_VALVE_HEATING: "switch",
+    TYPE_LIGHT_SCHEME: "button",
+}
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
@@ -40,7 +75,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         await hub.async_close()
         raise ConfigEntryNotReady(f"Unable to connect to Larnitech API2 at {host}") from exc
 
-    _cleanup_stale_generic_switch_entities(hass, hub.devices)
+    _cleanup_stale_hidden_entities(hass, hub.devices)
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = hub
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
@@ -60,18 +95,21 @@ async def _async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
     await hass.config_entries.async_reload(entry.entry_id)
 
 
-def _cleanup_stale_generic_switch_entities(
+def _cleanup_stale_hidden_entities(
     hass: HomeAssistant,
     devices: list[LarnitechDevice],
 ) -> None:
-    """Remove old entity-registry entries for hidden generic Larnitech switches."""
+    """Remove old entity-registry entries for low-level hidden Larnitech items."""
     registry = er.async_get(hass)
     for device in devices:
-        if device.type != TYPE_SWITCH:
+        if entity_enabled_default(device):
+            continue
+        entity_domain = TYPE_TO_ENTITY_DOMAIN.get(device.type)
+        if entity_domain is None:
             continue
         unique_id = f"{DOMAIN}_{device.addr.replace(':', '_')}"
-        entity_id = registry.async_get_entity_id("switch", DOMAIN, unique_id)
+        entity_id = registry.async_get_entity_id(entity_domain, DOMAIN, unique_id)
         if entity_id is None:
             continue
         registry.async_remove(entity_id)
-        _LOGGER.debug("Removed stale Larnitech generic switch entity %s", entity_id)
+        _LOGGER.debug("Removed stale hidden Larnitech entity %s", entity_id)
